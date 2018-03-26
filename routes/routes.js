2249b8c711a5d65e3fb79f8859e724bd5c997ec9
Michael <michael@gene.co.uk>
2018-03-26 15:34:15 +0100

    create unique id and timestamp

--- routes/routes.js
+++ routes/routes.js
@@ -14,7 +14,9 @@ module.exports = function(app, io) {
 
     app.get('/report', (req, res) => {
 
+        site.time = +new Date();
         site.url = req.query.url;
+        site.id = uuidv4();
 
         res.render('../views/pages/sent');
         res.end();
@@ -70,3 +72,9 @@ module.exports = function(app, io) {
         return res.render('../views/pages/index');
     });
 };
+
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+}
