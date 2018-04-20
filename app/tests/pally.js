584c8d8581370c6d0ed8dfe3cfe57e35aa7ce943
Michael <michael@gene.co.uk>
2018-04-20 07:58:52 +0100

    update pup

--- app/tests/pally.js
+++ app/tests/pally.js
@@ -1,23 +1,62 @@
-const pa11y = require('pa11y');
-const puppeteer = require('puppeteer');
-const browser = puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-});
-
 module.exports = function (url) {
     // run the tests and output the results in the console
     console.log('Running pa11y...');
 
-    return pa11y(url, {
-        browser: browser
-    }).then((results) => {
-        console.log('pa11y results');
-        console.log(results);
-        return results;
-    }).catch(error => {
-        console.log('pa11y error');
-        console.log(error);
-    });
+    const pa11y = require('pa11y');
+    const puppeteer = require('puppeteer');
 
-    browser.close();
+    // Async function required for us to use await
+    async function runExample() {
+        let browser;
+        let pages;
+        try {
+
+            // Launch our own browser
+            browser = await puppeteer.launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+
+            const page = await browser.newPage();
+
+            console.log(1);
+
+            // Test http://example.com/ with our shared browser
+            const result1 = await pa11y(url, {
+                browser: browser,
+                page: page
+            });
+
+            console.log(2);
+
+            // Output the raw result objects
+            console.log(result1);
+
+            return result1;
+
+            // Close the browser instance and pages now we're done with it
+            for (const page of pages) {
+                await page.close();
+            }
+            await browser.close();
+
+        } catch (error) {
+
+            // Output an error if it occurred
+            console.error(error.message);
+
+            // Close the browser instance and pages if theys exist
+            if (pages) {
+                for (const page of pages) {
+                    await page.close();
+                }
+            }
+            if (browser) {
+                await browser.close();
+            }
+
+        }
+    }
+
+
+    return runExample();
 };
